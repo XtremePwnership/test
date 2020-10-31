753186b8698f91b88116b8a1460e90e46c8a2e47
FirozeJi <firozecool@gmail.com>
2020-10-31 22:10:24 +0530

    a testing deploy.bat file

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,6 @@
 set /p commit_msg="Commit message: "
 git add .
 git commit -m "a %commit_msg%"
+IF "%commit_msg%"=="" (git commit -m "whateva") ELSE (git commit -m "%commit_msg%")
 git push origin main
 PAUSE
